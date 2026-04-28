d3f5216cf111610dd3c070245e1c9cf43d2f1e65
Prashant Kumar <Prashant_Kumar23@dell.com>
2026-04-28 18:32:28 +0530

    Register Scan method

--- HealthScanAPI/Controllers/HealthScanController.cs
+++ HealthScanAPI/Controllers/HealthScanController.cs
@@ -12,7 +12,7 @@ namespace HealthScanAPI.Controllers
     {
         [HttpPost]
         [Route("registerUser")]
-        public IHttpActionResult Execute([FromBody] JObject data)
+        public IHttpActionResult RegisterUser([FromBody] JObject data)
         {
             if (data == null)
                 return BadRequest("Invalid JSON");
@@ -24,7 +24,7 @@ namespace HealthScanAPI.Controllers
                 new SqlParameter("@fname", data.SelectToken("personalInformation.name")?.Value<string>() ?? ""),
                 new SqlParameter("@lname", data.SelectToken("personalInformation.lname")?.Value<string>() ?? ""),
                 new SqlParameter("@rmobile", data.SelectToken("personalInformation.mobile")?.Value<string>() ?? ""),
-                new SqlParameter("@rDOB", data.SelectToken("personalInformation.rDOB")?.Value<string>() ?? ""),
+                new SqlParameter("@rDOB", data.SelectToken("personalInformation.DOB")?.Value<string>() ?? ""),
                 new SqlParameter("@remail", data.SelectToken("personalInformation.email")?.Value<string>() ?? ""),
                 new SqlParameter("@rextmobile", data.SelectToken("personalInformation.mobile")?.Value<string>() ?? ""),
                 new SqlParameter("@gender", data.SelectToken("personalInformation.gender")?.Value<string>() ?? ""),
@@ -35,6 +35,38 @@ namespace HealthScanAPI.Controllers
 
 
             var result = CommonStoredProcedureMethod("usp_registeruser", parameters.ToArray());
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("registerScan")]
+        public IHttpActionResult RegisterScan([FromBody] JObject data)
+        {
+            if (data == null)
+                return BadRequest("Invalid JSON");
+
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@regid", data.SelectToken("personalInformation.regid")?.Value<string>() ?? "SAUDI"),
+                new SqlParameter("@deptid", data.SelectToken("personalInformation.deptid")?.Value<string>() ?? "SAUDI"),
+                new SqlParameter("@desgid", data.SelectToken("personalInformation.desgid")?.Value<string>() ?? "SAUDI"),
+                new SqlParameter("@descent", data.SelectToken("personalInformation.descent")?.Value<string>() ?? ""),
+                new SqlParameter("@httype", data.SelectToken("physicalTests.httype")?.Value<string>() ?? ""),
+                new SqlParameter("@htFt", data.SelectToken("physicalTests.height")?.Value<string>() ?? ""),
+                new SqlParameter("@htIn", data.SelectToken("physicalTests.height")?.Value<string>() ?? ""),
+                new SqlParameter("@wttype", data.SelectToken("physicalTest.weight")?.Value<string>() ?? ""),
+                new SqlParameter("@wt", data.SelectToken("physicalTest.weight")?.Value<string>() ?? ""),
+                new SqlParameter("@pulse", data.SelectToken("physicalTest.pulse")?.Value<string>() ?? ""),
+                new SqlParameter("@peak", data.SelectToken("physicalTest.peakFlow")?.Value<string>() ?? ""),
+                new SqlParameter("@waist", data.SelectToken("physicalTest.waist")?.Value<string>() ?? ""),
+                new SqlParameter("@breath", data.SelectToken("physicalTest.breathRetention")?.Value<string>() ?? ""),
+                new SqlParameter("@temp", data.SelectToken("physicalTest.temp")?.Value<string>() ?? ""),
+                new SqlParameter("@spo", data.SelectToken("physicalTest.spo")?.Value<string>() ?? "")
+            };
+
+
+            var result = CommonStoredProcedureMethod("usp_registerScan", parameters.ToArray());
 
             return Ok(result);
         }
